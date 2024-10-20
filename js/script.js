76000a24f8a4b133dfc2c72dad12687da3db4afa
Otavio <otaviomficho@gmail.com>
2024-10-19 22:47:35 -0300

    #

--- js/script.js
+++ js/script.js
@@ -5,6 +5,51 @@ document.addEventListener('DOMContentLoaded', function() {
 
     mobileBtn.addEventListener('click', function() {
         mobileMenu.classList.toggle('active');
+        icon.classList.toggle('fa-bars');
         icon.classList.toggle('fa-x');
     });
+
+    const sections = document.querySelectorAll('section');
+    const navItems = document.querySelectorAll('.nav-item');
+
+    window.addEventListener('scroll', function () {
+        const header = document.querySelector('header');
+        const scrollPosition = window.scrollY - header.offsetHeight;
+
+        let activeSectionIndex = 0;
+
+        if (scrollPosition <= 0) {
+            header.style.boxShadow = 'none';
+        } else {
+            header.style.boxShadow = '5px 1px 5px rgba(0, 0, 0, 0.1)';
+        }
+
+        sections.forEach((section, i) => {
+            const sectionTop = section.offsetTop - 100;
+            const sectionBottom = sectionTop + section.offsetHeight;
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = i;
+                return;
+            }
+        });
+
+        navItems.forEach((navItem) => {
+            navItem.classList.remove('active');
+        });
+        navItems[activeSectionIndex].classList.add('active');
+    });
+
+    ScrollReveal({
+        reset: true,
+        distance: '10px',
+        duration: 2000,
+        delay: 100
+    });
+
+    ScrollReveal().reveal('#cta', { origin: 'left' });
+    ScrollReveal().reveal('#banner', { origin: 'right' });
+    ScrollReveal().reveal('.dish', { origin: 'right' });
+    ScrollReveal().reveal('#testimonials_content', { origin: 'right' });
+    ScrollReveal().reveal('#testimonial_chef', { origin: 'left' });
 });
